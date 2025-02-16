f08175dc8d25ef3360ed786d97d03c35c675450d
metzys <contact@metzys.net>
2025-02-16 17:11:55 +0100

    changelang

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -5,7 +5,11 @@ const Navbar = (props: {
   return (
     <nav className="flex justify-between p-2 bg-slate-500">
       <span>icon</span>
-      <select name="lang" id="lang">
+      <select
+        name="lang"
+        id="lang"
+        onChange={(e) => props.changeLang(e.target.value)}
+      >
         <option value="fr">FR</option>
         <option value="en">EN</option>
       </select>
