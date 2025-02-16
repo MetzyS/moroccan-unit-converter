1b166e19eb63e4685c170b56839d5f57c078a1df
metzys <contact@metzys.net>
2025-02-16 17:10:50 +0100

    css

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -3,7 +3,7 @@ const Navbar = (props: {
   changeLang: (value: string) => void;
 }) => {
   return (
-    <nav className="flex">
+    <nav className="flex justify-between p-2 bg-slate-500">
       <span>icon</span>
       <select name="lang" id="lang">
         <option value="fr">FR</option>
