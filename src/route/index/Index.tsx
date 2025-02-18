499a75fe2b043c1b4578d17a6ac0a95873c4524e
metzys <contact@metzys.net>
2025-02-18 21:51:44 +0100

    page structure

--- src/route/index/Index.tsx
+++ src/route/index/Index.tsx
@@ -1,3 +1,5 @@
+import CurrencyTable from "../../component/currencyTable/CurrencyTable";
+import InputTable from "../../component/inputTable/InputTable";
 import LoadingSpinner from "../../component/ui/LoadingSpinner";
 import Tag from "../../component/ui/Tag";
 import { useLayoutContext } from "../../context/Layout";
@@ -13,42 +15,55 @@ const Index = () => {
   console.log(content);
 
   return (
-    <div className="mt-8">
+    <>
       {isLoading ? (
         <>
           <LoadingSpinner lang={lang} />
         </>
       ) : (
         <>
-          <h1 className="capitalize">{content.title}</h1>
-          <p className="first-letter:capitalize text-pretty">
-            {content.subtitle}
-          </p>
-          <ul className="flex gap-2 mt-2 relative">
-            {content.currencies.map((item, index) => {
-              return (
-                <Tag
-                  color={item.color}
-                  name={item.name}
-                  tooltipText={item.desc}
-                  key={`tag-${index}`}
-                />
-              );
-            })}
-          </ul>
-          <ul className="mt-6">
-            {content.currenciesDef.map((item, index) => (
-              <li
-                key={`currencyDef-${index}`}
-                className="list-disc list-inside"
-              >
-                {item.name}
-              </li>
-            ))}
-          </ul>
+          <section className="my-12">
+            <h1 className="capitalize">{content.title}</h1>
+            <p className="first-letter:capitalize text-pretty">
+              {content.subtitle}
+            </p>
+            <ul className="flex gap-2 mt-2 relative">
+              {content.currencies.map((item, index) => {
+                return (
+                  <Tag
+                    color={item.color}
+                    name={item.name}
+                    tooltipText={item.desc}
+                    key={`tag-${index}`}
+                  />
+                );
+              })}
+            </ul>
+          </section>
+          <section className="my-12">
+            <h2 className="first-letter:capitalize">
+              {content.converterTitle}
+            </h2>
+            <p className="first-letter:capitalize mb-4">
+              {content.converterText}
+            </p>
+            <InputTable
+              currenciesDef={content.currenciesDef}
+              mainCurrency={content.mainCurrency}
+            />
+          </section>
+          <section className="my-12">
+            <h2 className="mb-6 first-letter:capitalize">
+              {content.detailTitle}
+            </h2>
+            <CurrencyTable
+              currencies={content.currencies}
+              currenciesDef={content.currenciesDef}
+            />
+          </section>
         </>
       )}
-    </div>
+    </>
   );
 };
 
