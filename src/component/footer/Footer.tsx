b556081e9f8ca1a2407271fabcc6c9caaf07043a
metzys <contact@metzys.net>
2025-02-18 23:27:53 +0100

    footer component

--- src/component/footer/Footer.tsx
+++ src/component/footer/Footer.tsx
@@ -1,5 +1,28 @@
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+import MetzysIcon from "../ui/MetzysIcon";
+
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="flex justify-center p-2 bg-black/30">
+      <ul className="flex gap-4 items-center">
+        <li>
+          <a href="https://www.github.com/metzys" aria-label="github link">
+            <BsGithub size={20} />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.metzys.net" aria-label="portfolio link">
+            <MetzysIcon className="size-5" color="fill-white/60" />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/adnene-hamzaoui">
+            <BsLinkedin size={20} />
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
 };
 
 export default Footer;
