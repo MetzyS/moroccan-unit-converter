04b63de55f9aaaed04db9a2ac9ebec5717148618
metzys <contact@metzys.net>
2025-02-18 21:50:26 +0100

    input component

--- src/component/ui/InputCurrency.tsx
+++ src/component/ui/InputCurrency.tsx
@@ -1,10 +1,24 @@
-const InputCurrency = (props: { label: string; currency: string }) => {
+const InputCurrency = (props: {
+  label: string;
+  currency: string;
+  labelFirst?: boolean;
+  className?: string;
+}) => {
   return (
-    <div>
-      <label htmlFor={props.label}>
-        <input type="number" id={props.label} />
-      </label>
-    </div>
+    <label
+      htmlFor={props.currency}
+      className={`flex items-center ${props.className && props.className}`}
+    >
+      {props.labelFirst && (
+        <span className="capitalize mr-2">{props.label}</span>
+      )}
+      <input
+        type="number"
+        id={props.currency}
+        className="w-30 input-nb border border-white/70 mr-2"
+      />
+      {!props.labelFirst && <span className="capitalize">{props.label}</span>}
+    </label>
   );
 };
 
